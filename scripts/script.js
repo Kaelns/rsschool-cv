5db97465519e92b4b116ebdd037377c2d5a22088
Yury Ahanouski <uraaru@live.com>
2021-09-20 20:33:53 +0300

    fix: burger

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,15 @@
+const TABLET_SCREEN_WIDTH = 1024;
 const menuBtn = document.querySelector(".burger-btn");
 const headerNav = document.querySelector(".header__nav");
 
-document.addEventListener("DOMContentLoaded", () => {
-  headerNav.classList.add("js-hide-nav", "js-mobile-nav");
-});
-
 menuBtn.addEventListener("click", () => {
   headerNav.classList.toggle("js-hide-nav");
-  menuBtn.classList.toggle("js-open-burger");
 });
 
-const closeMenu = (event) => {
-  if (event.target.tagName !== "BUTTON") {
+const resizeHandler = (event) => {
+  if (window.innerWidth <= TABLET_SCREEN_WIDTH) {
     headerNav.classList.add("js-hide-nav");
-    menuBtn.classList.remove("js-open-burger");
   }
 };
 
-document.addEventListener("click", closeMenu);
+window.addEventListener("resize", resizeHandler);
